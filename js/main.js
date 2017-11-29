548d3b1bb6c024da69482cc7912f9a42ce9920f6
Lumas <lumas71@gmail.com>
2017-11-28 23:16:40 -0500

    Update Rocket Launcher
    Content for the rocket launcher sequence created.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+
+// This Function changes the class of an element
+var doCoolStuff = function() {
+	var currentClassName = document.getElementById('cool').className;
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+	console.log(currentClassName);
+}
+
 var car = {
 	make: 'VW',
 	type: 'Golf',
